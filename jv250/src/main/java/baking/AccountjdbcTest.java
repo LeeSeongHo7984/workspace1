d018079bbe3c67686bbb783433382a20a7dbfe8c
sh <tjdgh7984@naver.com>
2022-06-22 17:23:17 +0900

    u
    u

--- jv250/src/main/java/baking/AccountjdbcTest.java
+++ jv250/src/main/java/baking/AccountjdbcTest.java
@@ -1,5 +1,27 @@
 package baking;
 
-public class AccountjdbcTest {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class AccountjdbcTest {
+	public static void main(String[] args) {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/jv250?serverTimezone=Asia/Seoul";	// 서버주소
+		String id = "jv250";
+		String passwd = "jv250";
+		
+		try {
+			Class.forName(driver);
+			System.out.println("LOAD DRIVER --->" + driver);
+		
+			Connection con = DriverManager.getConnection(url, id, passwd);	
+			System.out.println("CONNECTED TO --->" + url);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void insert() {
+		
+	}
 }
